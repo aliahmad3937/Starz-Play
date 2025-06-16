abd27f20470b278f3d6621d391c6186f42b7c400
Ali Ahmad <91466169+aliahmad3937@users.noreply.github.com>
2025-06-16 20:37:16 +0500

    Add files via upload

---
+++ core/src/test/java/com/ali/starzplay/core/ExampleCoreUnitTest.kt
@@ -0,0 +1,11 @@
+package com.ali.starzplay.core
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class ExampleCoreUnitTest {
+    @Test
+    fun sampleTest() {
+        assertTrue(true)
+    }
+} 
